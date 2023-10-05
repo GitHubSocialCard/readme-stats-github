2830bec6401b3d9b1e9664a3dda61a1a4c39e135
FajarKim <PenghapusJawaban@gmail.com>
2023-10-05 18:39:28 +0700

    Update files

--- src/helper/template.ts
+++ src/helper/template.ts
@@ -87,7 +87,7 @@ export default function template(data: GetData, uiConfig: UiConfig): string {
         }
     </style>
 
-    <rect x="0.5" y="0.5" rx="4.5" height="99%" width="494" fill="${uiConfig.bgColor}" stroke="${uiConfig.borderColor}" stroke-opacity="1" />
+    <rect x="0.5" y="0.5" rx="4.5" height="99%" width="494" fill="${uiConfig.bgColor}" stroke="${uiConfig.borderColor}" stroke-width="1.5" stroke-opacity="1" />
     <g transform="translate(0, 25)">
         <g class="leftdiv">
         	<text x="10" y="-10" class="namepl">${data.name}</text>
@@ -104,7 +104,7 @@ export default function template(data: GetData, uiConfig: UiConfig): string {
             <svg x="52.4" y="126.8" class="icon" viewBox="0 0 18 18" version="1.1" width="18" height="18">
                 <path fill-rule="evenodd" d="M5.5 3.5a2 2 0 100 4 2 2 0 000-4zM2 5.5a3.5 3.5 0 115.898 2.549 5.507 5.507 0 013.034 4.084.75.75 0 11-1.482.235 4.001 4.001 0 00-7.9 0 .75.75 0 01-1.482-.236A5.507 5.507 0 013.102 8.05 3.49 3.49 0 012 5.5zM11 4a.75.75 0 100 1.5 1.5 1.5 0 01.666 2.844.75.75 0 00-.416.672v.352a.75.75 0 00.574.73c1.2.289 2.162 1.2 2.522 2.372a.75.75 0 101.434-.44 5.01 5.01 0 00-2.56-3.012A3 3 0 0011 4z" />
             </svg>
-            <text x="104" y="117" class="uname">${data.username}</text>
+            <text x="104" y="117" class="uname">@${data.username}</text>
             <text x="72.5" y="140" class="followers">Followers:</text>
             <text x="134.5" y="140" style="font-weight: 700" class="followers">${data.followers}</text>
         </g>
